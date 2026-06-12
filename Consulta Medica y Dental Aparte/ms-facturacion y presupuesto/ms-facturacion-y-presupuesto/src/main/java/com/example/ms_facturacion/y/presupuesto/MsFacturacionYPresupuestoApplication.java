b3fc42c77cebe38083f3891ef5c0d21a178071bb
javiera-faundez <ja.faundezb@duocuc.cl>
2026-06-11 23:28:28 -0400

    feat: update facturacion y presupuesto

--- Consulta Medica y Dental Aparte/ms-facturacion y presupuesto/ms-facturacion-y-presupuesto/src/main/java/com/example/ms_facturacion/y/presupuesto/MsFacturacionYPresupuestoApplication.java
+++ Consulta Medica y Dental Aparte/ms-facturacion y presupuesto/ms-facturacion-y-presupuesto/src/main/java/com/example/ms_facturacion/y/presupuesto/MsFacturacionYPresupuestoApplication.java
@@ -1,13 +1,15 @@
 package com.example.ms_facturacion.y.presupuesto;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ImportAutoConfiguration(exclude = {
+    org.springdoc.core.configuration.SpringDocHateoasConfiguration.class
+})
 public class MsFacturacionYPresupuestoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsFacturacionYPresupuestoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsFacturacionYPresupuestoApplication.class, args);
+    }
 }
