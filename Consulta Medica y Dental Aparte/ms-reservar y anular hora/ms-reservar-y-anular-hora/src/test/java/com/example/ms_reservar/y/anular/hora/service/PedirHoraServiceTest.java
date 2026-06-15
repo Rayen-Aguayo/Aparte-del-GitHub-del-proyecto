c913f4abdac77ee34b6cb2eefd25b1152df148cc
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-06-15 14:57:38 -0400

    feat: update service

--- Consulta Medica y Dental Aparte/ms-reservar y anular hora/ms-reservar-y-anular-hora/src/test/java/com/example/ms_reservar/y/anular/hora/service/PedirHoraServiceTest.java
+++ Consulta Medica y Dental Aparte/ms-reservar y anular hora/ms-reservar-y-anular-hora/src/test/java/com/example/ms_reservar/y/anular/hora/service/PedirHoraServiceTest.java
@@ -26,7 +26,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class PedirHoraServiceTest {
+public class PedirHoraServiceTest {
 
     @Mock
     private PedirHoraRepository repo;
