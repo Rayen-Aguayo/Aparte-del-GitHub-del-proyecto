378d5002c4e443fa3ed477ec697795db9e502d5f
javiera-faundez <ja.faundezb@duocuc.cl>
2026-06-18 00:22:12 -0400

    feat:update opinon del paciente

--- Consulta Medica y Dental Aparte/ms-opinion del paciente/ms-opinion-del-paciente/src/main/java/com/example/ms_opinion/del/paciente/MsOpinionDelPacienteApplication.java
+++ Consulta Medica y Dental Aparte/ms-opinion del paciente/ms-opinion-del-paciente/src/main/java/com/example/ms_opinion/del/paciente/MsOpinionDelPacienteApplication.java
@@ -3,11 +3,11 @@ package com.example.ms_opinion.del.paciente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springdoc.core.configuration.SpringDocHateoasConfiguration.class
+})
 public class MsOpinionDelPacienteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsOpinionDelPacienteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsOpinionDelPacienteApplication.class, args);
+    }
 }
