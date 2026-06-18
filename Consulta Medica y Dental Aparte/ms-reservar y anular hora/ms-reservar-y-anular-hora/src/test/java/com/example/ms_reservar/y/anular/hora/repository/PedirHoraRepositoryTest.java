2dd926e78eb67bd68ae0ab1ee2f5266ef81d915a
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-06-17 23:17:53 -0400

    repository

--- Consulta Medica y Dental Aparte/ms-reservar y anular hora/ms-reservar-y-anular-hora/src/test/java/com/example/ms_reservar/y/anular/hora/repository/PedirHoraRepositoryTest.java
+++ Consulta Medica y Dental Aparte/ms-reservar y anular hora/ms-reservar-y-anular-hora/src/test/java/com/example/ms_reservar/y/anular/hora/repository/PedirHoraRepositoryTest.java
@@ -3,7 +3,7 @@ package com.example.ms_reservar.y.anular.hora.repository;
 import com.example.ms_reservar.y.anular.hora.model.PedirHora;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
