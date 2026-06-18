9b0a52906a3d1e8e7ec048a89e840876fdb64834
javiera-faundez <ja.faundezb@duocuc.cl>
2026-06-18 00:42:10 -0400

    feat:update ficha medica

--- Consulta Medica y Dental Aparte/ms-ficha medica/ms-ficha-medica/src/main/java/com/example/ms_ficha/medica/MsFichaMedicaApplication.java
+++ Consulta Medica y Dental Aparte/ms-ficha medica/ms-ficha-medica/src/main/java/com/example/ms_ficha/medica/MsFichaMedicaApplication.java
@@ -3,7 +3,9 @@ package com.example.ms_ficha.medica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springdoc.core.configuration.SpringDocHateoasConfiguration.class
+})
 public class MsFichaMedicaApplication {
 
 	public static void main(String[] args) {
